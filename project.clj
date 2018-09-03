4bffd2118bcc29e33aaf17de5f29178eb27ea7b7
Christoph Frick <cf@ofnir.net>
2018-09-03 19:06:29 +0200

    Update versions

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject net.ofnir/wrepl "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [integrant "0.6.1"]
-                 [org.clojure/tools.cli "0.3.5"]]
+                 [integrant "0.6.3"]
+                 [org.clojure/tools.cli "0.3.7"]]
   :main wrepl.main
   :aot [wrepl.main])
