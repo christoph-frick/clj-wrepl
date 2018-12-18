a56547fc9a4a6e144f4b235e56307ed5b7e0c9f6
Christoph Frick <cf@ofnir.net>
2018-12-18 22:58:54 +0100

    Update to Clojure 1.10

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject net.ofnir/wrepl "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [integrant "0.6.3"]
                  [org.clojure/tools.cli "0.3.7"]]
   :main wrepl.main
