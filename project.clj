ed91ee5d5a9f9f3f16b66e0893568612ae08910d
Christoph Frick <cf@ofnir.net>
2020-02-11 12:15:14 +0100

    Version updates

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject net.ofnir/wrepl "0.1.0"
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [integrant "0.7.0"]
+                 [integrant "0.8.0"]
                  [com.cemerick/pomegranate "1.1.0"]
-                 [org.slf4j/slf4j-simple "1.7.29"]
+                 [org.slf4j/slf4j-simple "1.7.30"]
                  [org.clojure/tools.cli "0.4.2"]]
   :main wrepl.main
   :aot [wrepl.main]
