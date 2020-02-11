13de0f6ddb5220e8269ba8c1f2bddb696152e305
Christoph Frick <cf@ofnir.net>
2020-02-11 12:24:51 +0100

    Version 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject net.ofnir/wrepl "0.1.1-SNAPSHOT"
+(defproject net.ofnir/wrepl "0.1.1"
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [integrant "0.8.0"]
                  [com.cemerick/pomegranate "1.1.0"]
