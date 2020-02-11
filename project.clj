a2777a857d4298d3fbf750c22272edd92e075f44
Christoph Frick <cf@ofnir.net>
2020-02-11 12:21:02 +0100

    Bump to snapshot version and attempt to make `lein release` work again

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject net.ofnir/wrepl "0.1.0"
+(defproject net.ofnir/wrepl "0.1.1-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [integrant "0.8.0"]
                  [com.cemerick/pomegranate "1.1.0"]
@@ -7,7 +7,9 @@
   :main wrepl.main
   :aot [wrepl.main]
   :release-tasks [["vcs" "assert-committed"]
-                  ["change" "version"
-                   "leiningen.release/bump-version" "release"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
                   ["vcs" "commit"]
-                  ["vcs" "tag"]])
+                  ["vcs" "tag"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]])
