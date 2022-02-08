76ab9ca691f7d1542efef5e206c14815839ad57b
Christoph Frick <cf@ofnir.net>
2022-02-08 17:56:38 +0100

    Update deps

--- project.clj
+++ project.clj
@@ -1,11 +1,12 @@
 (defproject net.ofnir/wrepl "0.1.3-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
                  [integrant "0.8.0"]
                  [com.cemerick/pomegranate "1.1.0"]
-                 [org.slf4j/slf4j-simple "1.7.30"]
-                 [org.clojure/tools.cli "1.0.194"]]
+                 [org.slf4j/slf4j-simple "1.7.36"]
+                 [org.clojure/tools.cli "1.0.206"]]
   :main wrepl.main
   :aot [wrepl.main]
+  ; no release; binary gets built at github
   :release-tasks [["vcs" "assert-committed"]
                   ["change" "version" "leiningen.release/bump-version" "release"]
                   ["vcs" "commit"]
