71088938a53b19de1e3c444e182eb7bfe19ce568
Christoph Frick <cf@ofnir.net>
2022-06-08 20:48:34 +0200

    Add kaocha

--- project.clj
+++ project.clj
@@ -6,6 +6,11 @@
                  [org.clojure/tools.cli "1.0.206"]]
   :main wrepl.main
   :aot [wrepl.main]
+  :profiles {:kaocha {:dependencies [[lambdaisland/kaocha "1.66.1034"]]}}
+  :aliases {"test-refresh" ["kaocha" "--watch"]
+            "kaocha" ["with-profile" "+kaocha" "run" "-m" "kaocha.runner"]}
+  :global-vars {*warn-on-reflection* true
+                *unchecked-math* true}
   ; no release; binary gets built at github
   :release-tasks [["vcs" "assert-committed"]
                   ["change" "version" "leiningen.release/bump-version" "release"]
